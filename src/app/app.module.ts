9b8f5ed4f65984717004a404fac3943b0aa3fb08
Kunal Kumar <kmrknl2604@gmail.com>
2020-03-28 06:13:06 +0530

    Products feature route into App route (LAZY LOADING)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrishappMaterialModule } from './angular-material-module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TrishappMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
