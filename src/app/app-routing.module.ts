8e981fc6d13e125849afee567909da65c4f68703
Kunal Kumar <kmrknl2604@gmail.com>
2020-04-08 04:10:47 +0530

    Feature module : Auth module now contains Login,Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -21,12 +19,8 @@ const routes: Routes = [
     //loadChildren: './products/products.module#ProductsModule'  DEPRECATED in updated 'ng'
   },
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
